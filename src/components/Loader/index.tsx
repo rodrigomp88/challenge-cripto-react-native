9d199490578a021902c0013a4a8ca9195e652063
Rodrigo <rodrigomp88@gmail.com>
2022-11-15 18:30:50 -0300

    ready to merge

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import {LoadingText, LoadingWrapper} from './styles';
 
-const Loading = () => {
-  return (
-    <LoadingWrapper>
-      <LoadingText>Loading</LoadingText>
-    </LoadingWrapper>
-  );
-};
+const Loading = () => (
+  <LoadingWrapper>
+    <LoadingText>Loading</LoadingText>
+  </LoadingWrapper>
+);
 
 export default Loading;
